2d78ba25ce81337d2adcd45c2a26619bf2422209
aslamsm <aslamsm@yahoo.com>
2025-06-25 11:34:07 +0530

    Add files via upload

--- nodejs/asgmnt04.js
+++ nodejs/asgmnt04.js
@@ -1,10 +1,12 @@
 // factorial of number //
 let number = 5;
 let factorial = 1;
+let foutput = "";
 for (let i = 1; i <= number; i++) {
   factorial *= i;
-  console.log(`Factorial of ${i} is ${factorial}`);
+  foutput += `Factorial of ${i} is ${factorial}\n`;
 }
+console.log(foutput);
 // Fibonacci series //
 let n = 10; // Number of terms Fibonacci series
 let a = 0,
