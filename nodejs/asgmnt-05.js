5f2ff8bd69f07226e26b9e8c5a9d785d35f08f5c
aslamsm <aslamsm@yahoo.com>
2025-06-27 03:46:40 +0300

    Update asgmnt-05.js

--- nodejs/asgmnt-05.js
+++ nodejs/asgmnt-05.js
@@ -1,17 +1,19 @@
+// function to check Area of rectangle
 function calculateArea(length, width) {
   return length * width;
 }
 let area = calculateArea(5, 8);
 console.log(`Area of rectangle is : ${area}\n`);
 
+// function to check even/odd
 function checkEvenOdd(no) {
   let msg = no % 2 === 0 ? `No. ${no} is Even` : `No. ${no} is Odd\n`;
   console.log(msg);
 }
-
 checkEvenOdd(4);
 checkEvenOdd(7);
 
+// function to count Vowels
 function countVowels(ovstr) {
   let count = 0;
   let ch = "";
@@ -24,10 +26,9 @@ function countVowels(ovstr) {
   }
   console.log(`total vowels in [${ovstr}] : ${count}`);
 }
-
 countVowels("reactjs");
 countVowels("banana");
-
+// function to find Max number
 function finMax(a, b, c) {
   if (a >= b && a >= c) {
     console.log(`Max of ${a}, ${b}, ${c} is : ${a}`);
