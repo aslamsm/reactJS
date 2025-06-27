36e1c5ab696f025946a14c9544ad3ff3c9380fa3
aslamsm <aslamsm@yahoo.com>
2025-06-27 06:13:13 +0530

    Add files via upload

---
+++ nodejs/asgmnt-05.js
@@ -0,0 +1,51 @@
+function calculateArea(length, width) {
+  return length * width;
+}
+let area = calculateArea(5, 8);
+console.log(`Area of rectangle is : ${area}\n`);
+
+function checkEvenOdd(no) {
+  let msg = no % 2 === 0 ? `No. ${no} is Even` : `No. ${no} is Odd\n`;
+  console.log(msg);
+}
+
+checkEvenOdd(4);
+checkEvenOdd(7);
+
+function countVowels(ovstr) {
+  let count = 0;
+  let ch = "";
+
+  for (const chr of ovstr) {
+    ch = chr.toLowerCase();
+    if (ch == "a" || ch == "e" || ch == "i" || ch == "o" || ch == "u") {
+      count++;
+    }
+  }
+  console.log(`total vowels in [${ovstr}] : ${count}`);
+}
+
+countVowels("reactjs");
+countVowels("banana");
+
+function finMax(a, b, c) {
+  if (a >= b && a >= c) {
+    console.log(`Max of ${a}, ${b}, ${c} is : ${a}`);
+  } else if (b >= a && b >= c) {
+    console.log(`Max of ${a}, ${b}, ${c} is : ${b}`);
+  } else {
+    console.log(`Max of ${a}, ${b}, ${c} is : ${c}`);
+  }
+}
+finMax(12, 5, 20);
+
+function printPattern(chrs, no) {
+  for (i = 1; i <= no; i++) {
+    let prn = "";
+    for (j = 0; j < i; j++) {
+      prn = prn + `${chrs}`;
+    }
+    console.log(`${prn}`);
+  }
+}
+printPattern("*", 9);
