ab16711b5fd9dab4aac00d143b73f348d7eae8d2
aslamsm <aslamsm@yahoo.com>
2025-08-28 08:28:49 +0300

    Add files via upload

--- ShoppingCart/App.tsx
+++ ShoppingCart/App.tsx
@@ -1,26 +1,43 @@
 // src/App.tsx
-import React from "react";
 import { CartProvider } from "./components/CartContext/CartContext";
 import { ProductList } from "./components/CartContext/ProductList";
 import { Cart } from "./components/CartContext/Cart";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import OrderPlaced from "./components/CartContext/OrderPlaces";
+import AddressForm from "./components/CartContext/AddressForm";
 
-const App: React.FC = () => {
+function App() {
   return (
     <CartProvider>
-      <div className="container my-1">
-        <h3 style={{ marginLeft: "300px" }}>Buy Laptops Online</h3>
-
-        <div className="row">
-          <div className="col-md-8">
-            <ProductList />
+      <Router>
+        <nav className="navbar navbar-expand bg-danger px-0">
+          <Link className="navbar-brand text-white" to="/">
+            Shop
+          </Link>
+          <div className="navbar-nav">
+            <Link className="nav-link text-white" to="/products">
+              Products
+            </Link>
+            <Link className="nav-link text-white" to="/cart">
+              Cart
+            </Link>
+            <Link className="nav-link text-white" to="/order">
+              Order
+            </Link>
+            <Link className="nav-link text-white" to="/address">
+              Address
+            </Link>
           </div>
-          <div className="col-md-4">
-            <Cart />
-          </div>
-        </div>
-      </div>
+        </nav>
+        <Routes>
+          <Route path="/products" element={<ProductList />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/address" element={<AddressForm />} />
+          <Route path="/order" element={<OrderPlaced />} />
+        </Routes>
+      </Router>
     </CartProvider>
   );
-};
+}
 
 export default App;
